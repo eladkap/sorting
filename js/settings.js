1376646a91e0fed690325c8131b8b460571f18ae
eladkapuza <eladkapuza@gmail.com>
2024-03-18 19:35:41 +0200

    updated colors in settings

--- js/settings.js
+++ js/settings.js
@@ -5,25 +5,25 @@ var CANVAS_HEIGHT = 600;
 const FPS = 30;
 
 /* COLORS */
-const BLACK = [0, 0, 0];
-const GRAY1 = [50, 50, 50];
-const GRAY2 = [100, 100, 100];
-const GRAY3 = [200, 200, 200];
-const BLACKLIGHT = [100, 100, 100];
-const WHITE = [255, 255, 255];
-const RED = [200, 0, 0];
-const REDLIGHT = [200, 100, 100];
-const YELLOW = [255, 190, 0];
+const BLACK = 'rgb(0, 0, 0)';
+const GRAY1 = 'rgb(50, 50, 50)';
+const GRAY2 = 'rgb(100, 100, 100)';
+const GRAY3 = 'rgb(200, 200, 200)';
+const BLACKLIGHT = 'rgb(100, 100, 100)';
+const WHITE = 'rgb(255, 255, 255)';
+const RED = 'rgb(200, 0, 0)';
+const REDLIGHT = 'rgb(200, 100, 100)';
+const YELLOW = 'rgb(255, 190, 0)';
 const GREEN = 'rgb(0, 100, 0)';
-const GREENLIGHT = [50, 200, 50];
-const PINK = [255, 120, 255];
-const PURPLE = [255, 0, 255];
-const BLUEHEAVY = [50, 50, 205];
-const BLUE = [50, 50, 255];
-const BLUELIGHT = [100, 100, 255];
-const BROWN = [128, 64, 0];
-const BROWNLIGHT = [230, 115, 0];
-const TURQUOISE = [32, 114, 106];
+const GREENLIGHT = 'rgb(50, 200, 50)';
+const PINK = 'rgb(255, 120, 255)';
+const PURPLE = 'rgb(255, 0, 255)';
+const BLUEHEAVY = 'rgb(50, 50, 205)';
+const BLUE = 'rgb(50, 50, 255)';
+const BLUELIGHT = 'rgb([100, 100, 255)';
+const BROWN = 'rgb(128, 64, 0)';
+const BROWNLIGHT = 'rgb(230, 115, 0)';
+const TURQUOISE = 'rgb(32, 114, 106)';
 const TURQUOISELIGHT = 'rgb(50, 180, 166)';
 
 /* ARRAY */
